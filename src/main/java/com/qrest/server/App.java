26108c7940514eda03f0608e189a25878055d1d2
Archelunch <alivemihail@gmail.com>
2016-05-21 14:19:16 +0300

    t

--- src/main/java/com/qrest/server/App.java
+++ src/main/java/com/qrest/server/App.java
@@ -34,7 +34,6 @@ public class App implements Runnable
     private static Statement statement;
     private static String nid = "";
     private static String score = "0";
-    private int ze = 0;
     App(Socket clientSocket){
     	this.clientSocket = clientSocket;
     }
